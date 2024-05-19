ee2d512a798f3c080c7331c3d355dd8157b68c42
Isro Hidayatulloh <orsi.amax@gmail.com>
2024-05-20 00:29:04 +0700

    Login page

---
+++ lib/presentation/auth/widget/login_page.dart
@@ -0,0 +1,114 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_clinic_app/core/components/components.dart';
+import 'package:flutter_clinic_app/core/constants/colors.dart';
+import 'package:flutter_clinic_app/core/core.dart';
+import 'package:flutter_clinic_app/presentation/home/pages/dashboard.dart';
+import 'package:flutter_clinic_app/presentation/home/pages/main_nav_page.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      body: Row(children: [
+        // LEFT CONTENT
+        Expanded(
+          flex: 1,
+          child: Align(
+            alignment: AlignmentDirectional.topStart,
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(24.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const SpaceHeight(80.0),
+                  const Center(
+                    child: Text(
+                      'Masuk Staff',
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const SpaceHeight(30.0),
+                  CustomTextField(controller: emailController, label: 'Email'),
+                  const SpaceHeight(20.0),
+                  CustomTextField(
+                    controller: passwordController,
+                    label: 'Kata Sandi',
+                  ),
+                  const SpaceHeight(40.0),
+                  Button.filled(
+                    onPressed: () {
+                      context.pushReplacement(const MainNavPage());
+                    },
+                    label: 'MASUK',
+                  ),
+                  const SpaceHeight(20.0),
+                  const SpaceHeight(100.0),
+                  const Text(
+                    '© 2024 FIC 15 | ORSI CLINIC',
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        // RIGHT CONTENT
+        Expanded(
+          flex: 1,
+          child: Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              width: context.deviceHeight,
+              height: context.deviceHeight,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: Assets.images.splashScreen.provider(),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  top: 67.0,
+                  right: 15.0,
+                  left: 120.0,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    const Text(
+                      'Embark on efficiency with single click, discover the seamless world of clinic management!',
+                      style: TextStyle(
+                        fontSize: 32.0,
+                        fontWeight: FontWeight.w700,
+                        color: AppColors.white,
+                      ),
+                      textAlign: TextAlign.end,
+                    ),
+                    const SpaceHeight(50.0),
+                    Flexible(child: Assets.images.dokterku.image()),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ]),
+    ));
+  }
+}
---
+++ lib/presentation/home/pages/main_nav_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MainNavPage extends StatefulWidget {
+  const MainNavPage({super.key});
+
+  @override
+  State<MainNavPage> createState() => _MainNavPageState();
+}
+
+class _MainNavPageState extends State<MainNavPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
